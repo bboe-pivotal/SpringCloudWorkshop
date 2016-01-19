be0eef494b0f18ac075289ecd0ee218bc8b4b6d2
Chris DeLashmutt <cdelashmutt@pivotal.io>
2016-01-18 22:14:51 -0500

    Removing unused imports

--- session_05/lab_08/hello-world/hello-ui/src/main/java/io/pivotal/helloui/controller/SayHelloCommand.java
+++ session_05/lab_08/hello-world/hello-ui/src/main/java/io/pivotal/helloui/controller/SayHelloCommand.java
@@ -1,13 +1,11 @@
 package io.pivotal.helloui.controller;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import io.pivotal.helloui.domain.HelloResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Component
 public class SayHelloCommand {
